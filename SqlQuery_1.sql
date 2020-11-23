00bf19ba23280d33c4ed52600030b1a960df6cdc
Faith Magras <bdanielle092@gmail.com>
2020-11-23 11:31:14 -0600

    working music history

--- SqlQuery_1.sql
+++ SqlQuery_1.sql
@@ -1,1 +1,4 @@
-﻿
+﻿-- 1.Query all of the entries in the Genre table
+SELECT * from Genre;
+
+--2.Query all the entries in the Artist table and order by the artist's name. HINT: use the ORDER BY keywords
