eec9e825483e2281edcbc8322cea6a57020daee3
Faith Magras <bdanielle092@gmail.com>
2020-11-23 21:10:10 -0600

    completed muisc history

--- ch20.sql
+++ ch20.sql
@@ -26,7 +26,7 @@
  --6.Write a SELECT statement that lists the Albums with no songs
  --SELECT album.Title FROM Album album
  --LEFT JOIN Song song on song.AlbumId  = Album.id
- --WHERE song.Id is NULL;4
+ --WHERE song.Id is NULL;
 
 --7.Using the INSERT statement, add one of your favorite artists to the Artist table.
  --ex. INSERT INTO Genre (Label) VALUES ('Techno');
@@ -44,18 +44,60 @@
  --Select * FROM Song;
 
  --10. Write a SELECT query that provides the song titles, album title, and artist name for all of the data you just entered in. Use the LEFT JOIN keyword sequence to connect the tables, and the WHERE keyword to filter the results to the album and artist you added.
- SELECT song.Title, album.Title, artist.ArtistName FROM Album
- LEFT JOIN Song on song.AlbumId = Album.Id
- LEFT JOIN Artist on song.ArtistId = Artist.Id
- WHERE Album.Title = 'Unorthodox JukeBox';
+ --SELECT song.Title, album.Title, artist.ArtistName FROM Album
+ --LEFT JOIN Song on song.AlbumId = Album.Id
+ --LEFT JOIN Artist on song.ArtistId = Artist.Id
+ --WHERE Album.Title = 'Unorthodox JukeBox';
 
-  select song.title, album.title, artist.artistname from Song
- left join Album on song.albumid = album.id
- left join artist on song.artistid = artist.id
- where Album.title = 'unorthodox jukebox';
+ -- select song.title, album.title, artist.artistname from Song
+ --left join Album on song.albumid = album.id
+ --left join artist on song.artistid = artist.id
+ --where Album.title = 'unorthodox jukebox';
 
+ --11.Write a SELECT statement to display how many songs exist for each album. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+ -- the first Album.Title is to count the songs then second it to add the titles. We are grouping the songs by album.title
+-- SELECT COUNT (Album.Title), Album.Title
+--FROM Song
+--LEFT JOIN Album on Song.AlbumId = Album.Id
+--GROUP BY Album.Title
 
+--12. Write a SELECT statement to display how many songs exist for each artist. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+-- SELECT COUNT (Artist.ArtistName), Artist.ArtistName
+--FROM Song
+--LEFT JOIN Artist on Song.ArtistId = Artist.Id
+--GROUP BY Artist.ArtistName
 
+--13.Write a SELECT statement to display how many songs exist for each genre. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+-- SELECT COUNT (Genre.Label), Genre.Label
+--FROM Song
+--LEFT JOIN Genre on Song.GenreId = Genre.Id
+--GROUP BY Genre.Label
+
+--14.Write a SELECT query that lists the Artists that have put out records on more than one record label. Hint: When using GROUP BY instead of using a WHERE clause, use the HAVING keyword
+--having count (distinct Label) > 1 to see which artist has more then one record label
+--SELECT Artist.ArtistName
+--FROM Album
+--LEFT JOIN Artist on Album.ArtistId = Artist.Id
+--GROUP BY Artist.ArtistName
+--HAVING COUNT (DISTINCT Label) > 1
+
+--15.Using MAX() function, write a select statement to find the album with the longest duration. The result should display the album title and the duration.
+--We are grabbing both albumLength and title from ablum then using the Where clauses to only display the longest duration 
+--SELECT AlbumLength, Title
+--FROM Album
+--WHERE AlbumLength = (SELECT MAX(AlbumLength)
+--FROM Album);
+
+--16.Using MAX() function, write a select statement to find the song with the longest duration. The result should display the song title and the duration.
+--SELECT SongLength, Title
+--FROM Song
+--WHERE SongLength = (SELECT MAX (SongLength) from Song);
+
+--17.Modify the previous query to also display the title of the album.
+SELECT SongLength, Song.Title, Album.Title
+FROM Song
+LEFT JOIN Album on song.AlbumId = Album.id
+WHERE SongLength = (SELECT MAX (SongLength) from Song);
 
 
 
